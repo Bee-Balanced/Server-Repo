037ab93fd2f486c6e0cc6d229bf740b6c8d68962
gannonrowlan <102391770+gannonrowlan@users.noreply.github.com>
2025-02-23 18:52:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,35 +9,25 @@ const PORT = 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-
-let userProgress = {};
-
-// Configure session
 app.use(session({
   secret: "your_secret_key", 
   resave: false, 
   saveUninitialized: true
 }));
 
+let userProgress = {};
+
 app.get("/", (req, res) => {
   res.redirect("/login");
-});
-
-// home page, to start surveys
-app.get("/home", (req, res) => {
-  res.render("home");
-});
-
-// surveys
-app.get("/survey", (req, res) => {
-  const section = req.query.section || "general"; // Default to 'general'
-  res.render("survey", { section });
 });
 
 // Route for login ("/login")
 app.get("/login", (req, res) => {
   res.render("login");
 });
+
+// Handle POST request for login
+app.post("/login", handleLogin);
 
 // logout, goes back to login page
 app.get("/logout", (req, res) => {
@@ -50,11 +40,19 @@ app.get("/signup", (reg, res) => {
   res.render("signup");
 });
 
-// Handle POST request for login
-app.post("/login", handleLogin);
-
 // Handle POST request for signup
 app.post("/signup", handleSignup);
+
+// home page, to start surveys
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+// surveys
+app.get("/survey", (req, res) => {
+  const section = req.query.section || "general"; // Default to 'general'
+  res.render("survey", { section });
+});
 
 app.get("/survey-choice", (req, res) => {
   res.render("survey-choice", { userProgress });
